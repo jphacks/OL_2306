8c8fb5afe5f7d59d5378db92093d94410ffe1de4
54chair <takukoshi3721@gmail.com>
2023-10-28 11:38:12 +0900

    ヘッダーをForm.tsxに追加

--- app/src/features/signin/Form.tsx
+++ app/src/features/signin/Form.tsx
@@ -1,14 +1,17 @@
 import type { FC } from 'react';
 import { Container, Input, FormControl, FormLabel, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
+import { Layout } from '@/application/UI/Components/layout';
 
 export const Form:FC = () => {
   return <>
-    <Container>
-      <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input type='email' />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-    </Container>
+    <Layout title="フォトマ">
+      <Container>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input type='email' />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+      </Container>
+    </Layout>
   </>;
 };
