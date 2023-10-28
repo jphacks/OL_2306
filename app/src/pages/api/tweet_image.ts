f971f11e28ec7939f463816affbfe33a87a1e5e3
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-28 23:14:31 +0900

    fix: lint解消

--- app/src/pages/api/tweet_image.ts
+++ app/src/pages/api/tweet_image.ts
@@ -1,5 +1,5 @@
-import mysql_connection from "@/application/lib/db/connect_db";
-import type { NextApiRequest, NextApiResponse } from "next";
+import mysql_connection from '@/application/lib/db/connect_db';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type TweetImage = {
   tweetId: number;
@@ -13,28 +13,28 @@ export default async function handler(
   // mysqlと接続
   const connection = await mysql_connection();
 
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const { tweetId, imagePath }: TweetImage = req.body;
 
     if (!tweetId || !imagePath) {
       return res.status(400).json({
         success: false,
-        message: "Required parameters are missing",
+        message: 'Required parameters are missing',
       });
     }
 
     try {
       const result = await connection.query(
-        "INSERT INTO tweet_images (tweet_id, image_path) VALUES (?, ?)",
+        'INSERT INTO tweet_images (tweet_id, image_path) VALUES (?, ?)',
         [tweetId, imagePath]
       );
 
       res
         .status(200)
-        .json({ message: "Data has been successfully stored.", result });
+        .json({ message: 'Data has been successfully stored.', result });
     } catch (error) {
       res.status(500).json({
-        message: "Adding data failed.",
+        message: 'Adding data failed.',
         error: error,
       });
     }
@@ -42,6 +42,6 @@ export default async function handler(
 
   return res.status(405).json({
     success: false,
-    message: "Method not allowed",
+    message: 'Method not allowed',
   });
 }
