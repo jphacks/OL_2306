e686bc49273e112eefce4bc5b51523bb3d9d3335
54chair <takukoshi3721@gmail.com>
2023-10-28 15:59:16 +0900

    stateでフォームの入力値を管理する

--- app/src/features/signin/UI/Presentational/SigninPre.tsx
+++ app/src/features/signin/UI/Presentational/SigninPre.tsx
@@ -1,5 +1,6 @@
-import type { FC } from 'react';
+import type { ChangeEvent, FC } from 'react';
 import { Flex, Stack, Box, Button, Text, Input, FormControl, FormLabel } from '@chakra-ui/react';
+import { useState } from 'react';
 import { Layout } from '@/application/UI/Components/layout';
 import Link from 'next/link';
 
@@ -11,7 +12,15 @@ interface SigninPreProps {
  * Presentational（サインイン画面のUIを記述する）
  * @returns 
  */
-export const SigninPre:FC<SigninPreProps> = ({}) => {
+export const SigninPre:FC<SigninPreProps> = () => {
+  const initialValues = { email: "", password: "" }
+  const [formValues, setFormValues] = useState<SigninPreProps>(initialValues)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value })
+    console.log(formValues);
+  }
   return <>
     <Layout title="フォトマ">
       <Flex
@@ -28,11 +37,21 @@ export const SigninPre:FC<SigninPreProps> = ({}) => {
             <Stack spacing={8}>
               <FormControl>
                 <FormLabel>メールアドレス</FormLabel>
-                <Input type="email" placeholder="メールアドレス" />
+                <Input
+                  type="email"
+                  name='email'
+                  placeholder="メールアドレス"
+                  onChange={(e) => handleChange(e)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>パスワード</FormLabel>
-                <Input type="password" placeholder="パスワード" />
+                <Input
+                  type="password"
+                  name='password'
+                  placeholder="パスワード"
+                  onChange={(e) => handleChange(e)}
+                 />
               </FormControl>
               <Link href="/signup"><Text color="blue.400" p={1}>新規登録へ</Text></Link>
               <Button
--- app/src/features/signup/UI/Presentational/SignupPre.tsx
+++ app/src/features/signup/UI/Presentational/SignupPre.tsx
@@ -1,4 +1,5 @@
-import type { FC } from 'react';
+import { useState } from 'react';
+import type { ChangeEvent, FC } from 'react';
 import { Flex, Stack, Box, Button, Text, Input, FormControl, FormLabel } from '@chakra-ui/react';
 import { Layout } from '@/application/UI/Components/layout';
 import Link from 'next/link';
@@ -8,10 +9,18 @@ interface SignupPreProps {
 }
 
 /**
- * Presentational（サインイン画面のUIを記述する）
+ * Presentational（サインアップ画面のUIを記述する）
  * @returns 
  */
-export const SignupPre:FC<SignupPreProps> = ({}) => {
+export const SignupPre:FC<SignupPreProps> = () => {
+  const initialValues = { email: "", password: "" }
+  const [formValues, setFormValues] = useState<SignupPreProps>(initialValues)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value })
+    console.log(formValues);
+  }
   return <>
     <Layout title="フォトマ">
       <Flex
@@ -28,15 +37,30 @@ export const SignupPre:FC<SignupPreProps> = ({}) => {
             <Stack spacing={8}>
               <FormControl>
                 <FormLabel>ユーザー名</FormLabel>
-                <Input type="text" placeholder="ユーザー名" />
+                <Input
+                  type="text"
+                  name="username"
+                  placeholder="ユーザー名"
+                  onChange={(e) => handleChange(e)} 
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>メールアドレス</FormLabel>
-                <Input type="email" placeholder="メールアドレス" />
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="メールアドレス"
+                  onChange={(e) => handleChange(e)}
+                />
               </FormControl>
               <FormControl>
                 <FormLabel>パスワード</FormLabel>
-                <Input type="password" placeholder="パスワード" />
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="パスワード"
+                  onChange={(e) => handleChange(e)}
+                 />
               </FormControl>
               <Link href="/signin"><Text color="blue.400" p={1}>ログインへ</Text></Link>
               <Button
