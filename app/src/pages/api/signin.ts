1de33f0001b71528787170f36b213163c6898adb
mao-takekida <mao_takekida@amp.i.kyoto-u.ac.jp>
2023-10-28 13:18:11 +0900

    lint:fix

--- app/src/pages/api/signin.ts
+++ app/src/pages/api/signin.ts
@@ -3,48 +3,48 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 // ログイン処理を行うAPI
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        // クエリーパラメータから取得
-        const email = req.query.email as string;
-        const password = req.query.password as string;
+  try {
+    // クエリーパラメータから取得
+    const email = req.query.email as string;
+    const password = req.query.password as string;
 
-        // パラメータが空でないかチェック
-        const check_parameter = (parameter_name: string, parameter: string) => {
-            if (!parameter) {
-                res.status(400).json({
-                    message: parameter_name + 'が空です。',
-                });
-                return false;
-            }
-            return true;
-        }
-        if (!check_parameter('email', email) || !check_parameter('password', password)) {
-            return;
-        }
-
-        // mysqlと接続
-        const connection = await mysql_connection();
-        // SQL実行
-        const result = await connection.query(
-            'SELECT * FROM users where email = ? AND password = ?',
-            [email, password]
-        );
-        connection.end();
-
-        // 結果を返す
-        console.log(result[0]);
-        res
-            .status(200)
-            .json({ message: '接続に成功しました。', contents: result[0] });
-    } catch (error) {
-        res.status(405).json({
-            message: '取得に失敗しました。',
-            error: error,
+    // パラメータが空でないかチェック
+    const check_parameter = (parameter_name: string, parameter: string) => {
+      if (!parameter) {
+        res.status(400).json({
+          message: parameter_name + 'が空です。',
         });
+        return false;
+      }
+      return true;
+    };
+    if (!check_parameter('email', email) || !check_parameter('password', password)) {
+      return;
     }
+
+    // mysqlと接続
+    const connection = await mysql_connection();
+    // SQL実行
+    const result = await connection.query(
+      'SELECT * FROM users where email = ? AND password = ?',
+      [email, password]
+    );
+    connection.end();
+
+    // 結果を返す
+    console.log(result[0]);
+    res
+      .status(200)
+      .json({ message: '接続に成功しました。', contents: result[0] });
+  } catch (error) {
+    res.status(405).json({
+      message: '取得に失敗しました。',
+      error: error,
+    });
+  }
 }
 
 
--- app/src/pages/api/signup.ts
+++ app/src/pages/api/signup.ts
@@ -1,55 +1,55 @@
-import mysql_connection from "@/application/lib/db/connect_db";
-import type { NextApiRequest, NextApiResponse } from "next";
+import mysql_connection from '@/application/lib/db/connect_db';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 // 新規登録処理を行うAPI
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        // クエリーパラメータから取得
-        const email = req.query.email as string;
-        const password = req.query.password as string;
-        const user_name = req.query.user_name as string;
-        var description = req.query.description as string;
-        var icon_path = req.query.icon_path as string;
+  try {
+    // クエリーパラメータから取得
+    const email = req.query.email as string;
+    const password = req.query.password as string;
+    const user_name = req.query.user_name as string;
+    let description = req.query.description as string;
+    let icon_path = req.query.icon_path as string;
 
-        // パラメータが空でないかチェック
-        const check_parameter = (parameter_name: string, parameter: string) => {
-            if (!parameter) {
-                res.status(400).json({
-                    message: parameter_name + "が空です。",
-                });
-                return false;
-            }
-            return true;
-        }
-        if (!check_parameter("email", email) || !check_parameter("password", password) || !check_parameter("user_name", user_name)) {
-            return;
-        }
-
-        // description と icon_path は NULL の場合　空文字に変換
-        if (!description) {
-            description = "";
-        }
-        if (!icon_path) {
-            icon_path = "";
-        }
-
-        // mysqlと接続
-        const connection = await mysql_connection();
-        // SQL実行
-        const result = await connection.query(
-            "INSERT INTO users (email, password, user_name, description, icon_path) VALUES (?, ?, ?, ?, ?)",
-            [email, password, user_name, description, icon_path]
-        );
-        connection.end();
-        // // レスポンスを返す
-        res.status(200).json({ message: "新規登録に成功しました。", contents: result[0] });
-    } catch (error) {
-        res.status(405).json({
-            message: "新規登録に失敗しました。",
-            error: error,
+    // パラメータが空でないかチェック
+    const check_parameter = (parameter_name: string, parameter: string) => {
+      if (!parameter) {
+        res.status(400).json({
+          message: parameter_name + 'が空です。',
         });
+        return false;
+      }
+      return true;
+    };
+    if (!check_parameter('email', email) || !check_parameter('password', password) || !check_parameter('user_name', user_name)) {
+      return;
     }
+
+    // description と icon_path は NULL の場合　空文字に変換
+    if (!description) {
+      description = '';
+    }
+    if (!icon_path) {
+      icon_path = '';
+    }
+
+    // mysqlと接続
+    const connection = await mysql_connection();
+    // SQL実行
+    const result = await connection.query(
+      'INSERT INTO users (email, password, user_name, description, icon_path) VALUES (?, ?, ?, ?, ?)',
+      [email, password, user_name, description, icon_path]
+    );
+    connection.end();
+    // // レスポンスを返す
+    res.status(200).json({ message: '新規登録に成功しました。', contents: result[0] });
+  } catch (error) {
+    res.status(405).json({
+      message: '新規登録に失敗しました。',
+      error: error,
+    });
+  }
 }
