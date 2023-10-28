ae70d93a25ecc0231aaa9bec6d227aa5cef3db6d
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-28 13:07:45 +0900

    fix: lintエラー解消

--- app/src/pages/api/tweet/delete/[postId].ts
+++ app/src/pages/api/tweet/delete/[postId].ts
@@ -1,19 +1,19 @@
-import mysql_connection from "@/application/lib/db/connect_db";
-import { NextApiRequest, NextApiResponse } from "next";
+import mysql_connection from '@/application/lib/db/connect_db';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "DELETE") {
-    return res.status(405).json({ message: "Method Not Allowed" });
+  if (req.method !== 'DELETE') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
   }
 
   const postId = req.query.postId as string;
   const { userId } = req.body;
 
   if (!postId || !userId) {
-    return res.status(400).json({ message: "Required parameters are missing" });
+    return res.status(400).json({ message: 'Required parameters are missing' });
   }
 
   try {
@@ -22,30 +22,30 @@ export default async function handler(
 
     // 論理削除
     const [result] = await connection.query(
-      "UPDATE tweet SET is_delete = true WHERE id = ? AND user_id = ?",
+      'UPDATE tweet SET is_delete = true WHERE id = ? AND user_id = ?',
       [postId, userId]
     );
 
     connection.end();
 
-    if (!("affectedRows" in result)) {
-      return res.status(500).json({ message: "Unexpected database response." });
+    if (!('affectedRows' in result)) {
+      return res.status(500).json({ message: 'Unexpected database response.' });
     }
 
     if (result.affectedRows === 0) {
       return res
         .status(404)
-        .json({ message: "No matching tweet found for deletion." });
+        .json({ message: 'No matching tweet found for deletion.' });
     }
 
-    res.status(200).json({ message: "Successfully deleted the tweet." });
+    res.status(200).json({ message: 'Successfully deleted the tweet.' });
   } catch (error: unknown) {
     if (error instanceof Error) {
       res
         .status(500)
-        .json({ message: "Failed to delete the tweet.", error: error.message });
+        .json({ message: 'Failed to delete the tweet.', error: error.message });
     } else {
-      res.status(500).json({ message: "Failed to delete the tweet." });
+      res.status(500).json({ message: 'Failed to delete the tweet.' });
     }
   }
 }
