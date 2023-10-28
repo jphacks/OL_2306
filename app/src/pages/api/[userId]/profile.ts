afa6982afe31419d7819f480b3b9ad8c729b43ec
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-28 14:35:11 +0900

    lint: 修正

--- app/src/pages/api/[userId]/profile.ts
+++ app/src/pages/api/[userId]/profile.ts
@@ -1,6 +1,6 @@
-import mysql_connection from "@/application/lib/db/connect_db";
-import { RowDataPacket } from "mysql2";
-import { NextApiRequest, NextApiResponse } from "next";
+import mysql_connection from '@/application/lib/db/connect_db';
+import type { RowDataPacket } from 'mysql2';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,23 +9,23 @@ export default async function handler(
   const userId = req.query.userId as string;
 
   if (!userId) {
-    res.status(400).json({ message: "User ID is missing" });
+    res.status(400).json({ message: 'User ID is missing' });
     return;
   }
 
   const connection = await mysql_connection();
 
   switch (req.method) {
-    case "GET":
+    case 'GET': {
       try {
         const userProfile = <RowDataPacket[]>(
-          await connection.query("SELECT description FROM users WHERE id = ?", [
+          await connection.query('SELECT description FROM users WHERE id = ?', [
             userId,
           ])
         );
 
         if (!userProfile.length) {
-          return res.status(404).json({ message: "User not found" });
+          return res.status(404).json({ message: 'User not found' });
         }
 
         res.status(200).json(userProfile[0]);
@@ -33,49 +33,52 @@ export default async function handler(
         handleDatabaseError(res, error);
       }
       break;
+    }
 
-    case "POST":
+    case 'POST': {
       const { content } = req.body;
 
       try {
         await connection.query(
-          "UPDATE users SET description = ? WHERE id = ?",
+          'UPDATE users SET description = ? WHERE id = ?',
           [content, userId]
         );
 
         res
           .status(200)
-          .json({ message: "Successfully created the profile description." });
+          .json({ message: 'Successfully created the profile description.' });
       } catch (error: unknown) {
         handleDatabaseError(res, error);
       }
       break;
+    }
 
-    case "PUT":
+    case 'PUT': {
       const updatedContent = req.body.content;
 
       try {
         await connection.query(
-          "UPDATE users SET description = ? WHERE id = ?",
+          'UPDATE users SET description = ? WHERE id = ?',
           [updatedContent, userId]
         );
 
         res
           .status(200)
-          .json({ message: "Successfully updated the profile description." });
+          .json({ message: 'Successfully updated the profile description.' });
       } catch (error: unknown) {
         handleDatabaseError(res, error);
       }
+    }
   }
 }
 
 function handleDatabaseError(res: NextApiResponse, error: unknown) {
   if (error instanceof Error) {
     res.status(500).json({
-      message: "Failed to process database request.",
+      message: 'Failed to process database request.',
       error: error.message,
     });
   } else {
-    res.status(500).json({ message: "Failed to process database request." });
+    res.status(500).json({ message: 'Failed to process database request.' });
   }
 }
