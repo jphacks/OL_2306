52ffd779b2ce7882e7e0934ecb9fc36cfbbfcc23
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-28 20:13:43 +0900

    lint: 解消

--- app/src/features/home/UI/Container/HomeCon.tsx
+++ app/src/features/home/UI/Container/HomeCon.tsx
@@ -1,29 +1,40 @@
-import { FC, useEffect } from "react";
-import { useState } from "react";
-import { HomePre } from "../Presentational/HomePre";
+import type { FC } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { HomePre } from '../Presentational/HomePre';
 
 /**
  * Container（ホーム画面のロジックを記述する）
  * @returns
  */
+
+type TweetType = {
+  content: string;
+  id: number;
+  user_id: number;
+  user_name: string;
+  type: 'tweet' | 'model' | 'camera';
+  // 他のプロパティがあればこちらに追加してください
+};
+
 export const HomeCon: FC = () => {
-  const [timeline, setTimeline] = useState<Array<any>>([]);
+  const [timeline, setTimeline] = useState<Array<TweetType>>([]);
   const [filteredType, setFilteredType] = useState<
-    "tweet" | "model" | "camera"
-  >("tweet");
+    'tweet' | 'model' | 'camera'
+  >('tweet');
 
   useEffect(() => {
     async function fetchTimeline() {
       try {
-        const response = await fetch("/api/timeline");
+        const response = await fetch('/api/timeline');
         const data = await response.json();
         if (response.ok && data.tweets && Array.isArray(data.tweets)) {
           setTimeline(data.tweets);
         } else {
-          console.error("API did not return the expected format:", data);
+          console.error('API did not return the expected format:', data);
         }
       } catch (error) {
-        console.error("Failed to fetch timeline:", error);
+        console.error('Failed to fetch timeline:', error);
       }
     }
 
