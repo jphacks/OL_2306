aaa7b690d41bb2dc793c9d1c88d0857630cafffc
54chair <takukoshi3721@gmail.com>
2023-10-28 17:44:48 +0900

    サインアップの処理の実装

--- app/src/features/signup/UI/Container/SignupCon.tsx
+++ app/src/features/signup/UI/Container/SignupCon.tsx
@@ -1,10 +1,24 @@
-import type { FC } from 'react';
+import { useState } from 'react';
+import type { ChangeEvent, FC } from 'react';
 import { SignupPre } from '../Presentational/SignupPre';
+
+export interface FormValues {
+  username: string,
+  email: string,
+  password: string,
+}
 
 /**
  * Container（サインイン画面のロジックを記述する）
  * @returns 
  */
 export const SignupCon:FC = () => {
-  return <SignupPre/>;
+  const initialValues = { username: "", email: "", password: "" }
+  const [formValues, setFormValues] = useState<FormValues>(initialValues)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value })
+  }
+  return <SignupPre handleChange={handleChange} formValues={formValues} />;
 };
--- app/src/features/signup/UI/Presentational/SignupPre.tsx
+++ app/src/features/signup/UI/Presentational/SignupPre.tsx
@@ -1,26 +1,22 @@
-import { useState } from 'react';
 import type { ChangeEvent, FC } from 'react';
 import { Flex, Stack, Box, Button, Text, Input, FormControl, FormLabel } from '@chakra-ui/react';
+import axios from 'axios';
 import { Layout } from '@/application/UI/Components/layout';
 import Link from 'next/link';
+import { FormValues } from '../Container/SignupCon';
 
 // 関数と変数をPresentationalに渡すために型を記述する
 interface SignupPreProps {
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void
+  formValues: FormValues
 }
 
 /**
  * Presentational（サインアップ画面のUIを記述する）
  * @returns 
  */
-export const SignupPre:FC<SignupPreProps> = () => {
-  const initialValues = { email: "", password: "" }
-  const [formValues, setFormValues] = useState<SignupPreProps>(initialValues)
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value })
-    console.log(formValues);
-  }
+export const SignupPre:FC<SignupPreProps> = ({ handleChange, formValues }) => {
+  
   return <>
     <Layout title="フォトマ">
       <Flex
@@ -33,7 +29,30 @@ export const SignupPre:FC<SignupPreProps> = () => {
           p={10}
           w={600}
         >
-          <form>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            console.log(formValues);
+            fetch('/api/signup', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(
+                {
+                  "user_name": formValues.username,
+                  "email": formValues.email,
+                  "password": formValues.password,
+                }
+              )
+            }).then((res) => {
+              console.log(res);
+              res.json().then((json) => {
+                console.log(json);
+              })
+            }).catch((error) => {
+              console.log(error);
+            })
+          }}>
             <Stack spacing={8}>
               <FormControl>
                 <FormLabel>ユーザー名</FormLabel>
@@ -42,6 +61,7 @@ export const SignupPre:FC<SignupPreProps> = () => {
                   name="username"
                   placeholder="ユーザー名"
                   onChange={(e) => handleChange(e)} 
+                  value={formValues.username}
                 />
               </FormControl>
               <FormControl>
@@ -51,6 +71,7 @@ export const SignupPre:FC<SignupPreProps> = () => {
                   name="email"
                   placeholder="メールアドレス"
                   onChange={(e) => handleChange(e)}
+                  value={formValues.email}
                 />
               </FormControl>
               <FormControl>
@@ -60,6 +81,7 @@ export const SignupPre:FC<SignupPreProps> = () => {
                   name="password"
                   placeholder="パスワード"
                   onChange={(e) => handleChange(e)}
+                  value={formValues.password}
                  />
               </FormControl>
               <Link href="/signin"><Text color="blue.400" p={1}>ログインへ</Text></Link>
