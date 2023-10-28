113c0a605125e1543bf10a8de459de835c44fc32
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-28 23:41:53 +0900

    feat: +ボタンで投稿用のモーダルが展開できるように実装

--- app/src/features/home/UI/Presentational/HomePre.tsx
+++ app/src/features/home/UI/Presentational/HomePre.tsx
@@ -1,23 +1,76 @@
-import { Layout } from '@/application/UI/Components/layout';
-import { Box, Button, Heading } from '@chakra-ui/react';
-import type { FC } from 'react';
+import { Layout } from "@/application/UI/Components/layout";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { FC, useState } from "react";
 
-// 関数と変数をPresentationalに渡すために型を記述する
 interface HomePreProps {
-    count : number,
-    handleClick : () => void
+  count: number;
+  handleClick: () => void;
 }
 
 /**
  * Presentational（画面のUIを記述する）
- * @returns 
+ * @returns
  */
-export const HomePre:FC<HomePreProps> = ({ count, handleClick }) => {
-  return <Layout title='フォトマ'>
-    <Heading>ホーム画面</Heading>
-    <Box>
-      <Box>カウント：{count}</Box>
-      <Button onClick={handleClick}>クリック！！</Button>
-    </Box>
-  </Layout>;
+export const HomePre: FC<HomePreProps> = ({ count, handleClick }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState("");
+  const [type, setType] = useState<"tweet" | "model" | "camera">("tweet");
+
+  const handleClose = () => setIsOpen(false);
+  const handleOpen = () => setIsOpen(true);
+  const handleSubmit = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Layout title="フォトマ">
+      <Button position="fixed" right="1rem" bottom="1rem" onClick={handleOpen}>
+        +
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>タイプ</FormLabel>
+              <Select
+                value={type}
+                onChange={(e) =>
+                  setType(e.target.value as "tweet" | "model" | "camera")
+                }
+              >
+                <option value="tweet">つぶやき</option>
+                <option value="model">モデル募集</option>
+                <option value="camera">撮影者募集</option>
+              </Select>
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>内容</FormLabel>
+              <Textarea
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </Layout>
+  );
 };
