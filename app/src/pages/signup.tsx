28780db98a6cbdfe31c26dea6d908c0f827af110
54chair <takukoshi3721@gmail.com>
2023-10-28 13:56:38 +0900

    サインアップフォームのUI作成

--- app/src/pages/signup.tsx
+++ app/src/pages/signup.tsx
@@ -1,7 +1,8 @@
+import { SignupForm } from '@/features/signup/SignupForm';
 import type { NextPage } from 'next';
 
 const Signup:NextPage = () => {
-  return <>新規登録ページ</>;
+  return <SignupForm />;
 };
 
 export default Signup;
