0a18640d80c9ae63426fee426154da3603c6ed49
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-28 16:42:55 +0900

    feat: typeによってフィルタリングした結果を表示できるように実装

--- app/src/features/home/UI/Container/HomeCon.tsx
+++ app/src/features/home/UI/Container/HomeCon.tsx
@@ -1,20 +1,40 @@
-import type { FC} from 'react';
-import { useState } from 'react';
-import { HomePre } from '../Presentational/HomePre';
+import { FC, useEffect } from "react";
+import { useState } from "react";
+import { HomePre } from "../Presentational/HomePre";
 
 /**
  * Container（ホーム画面のロジックを記述する）
- * @returns 
+ * @returns
  */
-export const HomeCon:FC = () => {
-  // 例：カウントアップのロジック
-  // 変数
-  const [count, setCount] = useState<number>(0);
-  // 関数
-  const handleClick = () => {
-    setCount(prev => prev + 1);
-  };
+export const HomeCon: FC = () => {
+  const [timeline, setTimeline] = useState<Array<any>>([]);
+  const [filteredType, setFilteredType] = useState<
+    "tweet" | "model" | "camera"
+  >("tweet");
 
-  // 関数と変数をPresentationalに渡す
-  return <HomePre count={count} handleClick={handleClick}/>;
+  useEffect(() => {
+    async function fetchTimeline() {
+      try {
+        const response = await fetch("/api/timeline");
+        const data = await response.json();
+        if (response.ok && data.tweets && Array.isArray(data.tweets)) {
+          setTimeline(data.tweets);
+        } else {
+          console.error("API did not return the expected format:", data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch timeline:", error);
+      }
+    }
+
+    fetchTimeline();
+  }, []);
+
+  return (
+    <HomePre
+      timeline={timeline}
+      filteredType={filteredType}
+      setFilteredType={setFilteredType}
+    />
+  );
 };
