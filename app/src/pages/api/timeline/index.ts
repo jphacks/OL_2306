87d0e7d1107c8af1ea417b6558d822816adaef86
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-29 13:09:41 +0900

    lint;

--- app/src/pages/api/timeline/index.ts
+++ app/src/pages/api/timeline/index.ts
@@ -1,5 +1,5 @@
-import mysql_connection from "@/application/lib/db/connect_db";
-import type { NextApiRequest, NextApiResponse } from "next";
+import mysql_connection from '@/application/lib/db/connect_db';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,16 +9,16 @@ export default async function handler(
     const connection = await mysql_connection();
 
     const result = await connection.query(
-      "SELECT tweet.id, tweet.user_id, tweet.content, tweet.type, tweet.image_path, users.user_name FROM tweet join users on tweet.user_id = users.id where is_delete = 0"
+      'SELECT tweet.id, tweet.user_id, tweet.content, tweet.type, tweet.image_path, users.user_name FROM tweet join users on tweet.user_id = users.id where is_delete = 0'
     );
 
     res.status(200).json({
-      message: "Data acquisition was successful.",
+      message: 'Data acquisition was successful.',
       tweets: result[0],
     });
   } catch (error) {
     res.status(500).json({
-      massage: "Failed to retrieve data.",
+      massage: 'Failed to retrieve data.',
       error: error,
     });
   }
--- app/src/pages/api/tweet/index.ts
+++ app/src/pages/api/tweet/index.ts
@@ -1,10 +1,10 @@
-import mysql_connection from "@/application/lib/db/connect_db";
-import type { NextApiRequest, NextApiResponse } from "next";
+import mysql_connection from '@/application/lib/db/connect_db';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Tweet = {
   userId: string;
   content: string;
-  type: "tweet" | "model" | "camera";
+  type: 'tweet' | 'model' | 'camera';
   imagePath?: string;
 };
 
@@ -15,35 +15,35 @@ export default async function handler(
   // mysqlと接続
   const connection = await mysql_connection();
 
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const { userId, content, type, imagePath }: Tweet = req.body;
 
     if (!userId || !content || !type) {
       return res.status(400).json({
         success: false,
-        message: "Required parameters are missing",
+        message: 'Required parameters are missing',
       });
     }
 
-    if (!["tweet", "model", "camera"].includes(type)) {
+    if (!['tweet', 'model', 'camera'].includes(type)) {
       return res.status(400).json({
         success: false,
-        message: "Invalid type parameter",
+        message: 'Invalid type parameter',
       });
     }
 
     try {
       const result = await connection.query(
-        "INSERT INTO tweet (user_id, content, type, image_path) VALUES (?, ?, ?, ?)",
+        'INSERT INTO tweet (user_id, content, type, image_path) VALUES (?, ?, ?, ?)',
         [userId, content, type, imagePath]
       );
 
       res
         .status(200)
-        .json({ message: "Data has been successfully stored.", result });
+        .json({ message: 'Data has been successfully stored.', result });
     } catch (error) {
       res.status(500).json({
-        message: "Adding data failed.",
+        message: 'Adding data failed.',
         error: error,
       });
     }
@@ -51,6 +51,6 @@ export default async function handler(
 
   return res.status(405).json({
     success: false,
-    message: "Method not allowed",
+    message: 'Method not allowed',
   });
 }
