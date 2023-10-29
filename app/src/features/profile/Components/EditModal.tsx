f3dae5cd59921d5ed502ca744b237a9845616dc8
takeki <takekidanmkk@gmail.com>
2023-10-29 11:50:37 +0900

    lint: エラー修正

--- app/src/features/profile/Components/EditModal.tsx
+++ app/src/features/profile/Components/EditModal.tsx
@@ -1,5 +1,5 @@
-import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, Box } from '@chakra-ui/react';
-import { FC } from 'react';
+import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody } from '@chakra-ui/react';
+import type { FC } from 'react';
 import { AiFillEdit } from 'react-icons/ai';
 import type { GetProfileAPIResponse } from '../types/GetProfileAPIResponse';
 import { SettingForm } from './SettingForm';
@@ -10,22 +10,22 @@ interface SettingModalProps {
     setUserInfo: (userInfo: GetProfileAPIResponse) => void
 }
 export const SettingModal: FC<SettingModalProps> = ({ userId, userInfo, setUserInfo }) => {
-    const { isOpen, onClose, onOpen } = useDisclosure();
-    return (
-        <>
-            <Button onClick={onOpen}>
-                <AiFillEdit />
-            </Button>
-            {/* モーダルの横幅を画面いっぱいに */}
-            <Modal isOpen={isOpen} onClose={onClose} >
-                <ModalOverlay />
-                <ModalContent h='80%' maxW='80%' >
-                    <ModalHeader />
-                    <ModalBody >
-                        <SettingForm userId={userId} userInfo={userInfo} setUserInfo={setUserInfo} onClose={onClose} />
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        </>
-    );
-}
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>
+        <AiFillEdit />
+      </Button>
+      {/* モーダルの横幅を画面いっぱいに */}
+      <Modal isOpen={isOpen} onClose={onClose} >
+        <ModalOverlay />
+        <ModalContent h='80%' maxW='80%' >
+          <ModalHeader />
+          <ModalBody >
+            <SettingForm userId={userId} userInfo={userInfo} setUserInfo={setUserInfo} onClose={onClose} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
