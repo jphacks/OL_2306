487a54a70a000bb1a1121436ed7fde6321828006
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2023-10-29 14:02:42 +0900

    feat; カードのクリックで詳細モーダルを表示

--- app/src/features/home/UI/Container/HomeCon.tsx
+++ app/src/features/home/UI/Container/HomeCon.tsx
@@ -1,7 +1,7 @@
-import type { FC } from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { HomePre } from '../Presentational/HomePre';
+import type { FC } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { HomePre } from "../Presentational/HomePre";
 
 /**
  * Container（ホーム画面のロジックを記述する）
@@ -13,24 +13,35 @@ type TweetType = {
   id: number;
   user_id: number;
   user_name: string;
-  type: 'tweet' | 'model' | 'camera';
+  type: "tweet" | "model" | "camera";
   image_path: string;
 };
 
 export const HomeCon: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [content, setContent] = useState('');
-  const [imagePath, setImagePath] = useState('');
-  const [type, setType] = useState<'tweet' | 'model' | 'camera'>('tweet');
+  const [content, setContent] = useState("");
+  const [imagePath, setImagePath] = useState("");
+  const [isDetailModalOpen, setDetailModalOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<TweetType | null>(null);
+  const [type, setType] = useState<"tweet" | "model" | "camera">("tweet");
   const [timeline, setTimeline] = useState<Array<TweetType>>([]);
-  const [filteredType, setFilteredType] = useState<TweetType['type']>('tweet');
+  const [filteredType, setFilteredType] = useState<TweetType["type"]>("tweet");
   const [refreshKey, setRefreshKey] = useState(0); //ページレンダリングのためのカウントキー
+
+  const openDetailModal = (item: TweetType) => {
+    setSelectedItem(item);
+    setDetailModalOpen(true);
+  };
+
+  const closeDetailModal = () => {
+    setDetailModalOpen(false);
+  };
 
   const onClose = () => {
     setIsOpen(false);
-    setContent('');
-    setImagePath('');
-    setType('tweet');
+    setContent("");
+    setImagePath("");
+    setType("tweet");
   };
 
   const onOpen = () => {
@@ -43,29 +54,29 @@ export const HomeCon: FC = () => {
     imagePath: string
   ) => {
     try {
-      const response = await fetch('/api/tweet', {
-        method: 'POST',
+      const response = await fetch("/api/tweet", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ userId: '2', content, type, imagePath }), // userIdは仮
+        body: JSON.stringify({ userId: "2", content, type, imagePath }), // userIdは仮
       });
 
       const data = await response.json();
 
       if (response.ok) {
-        console.log('Posted successfully:', data);
+        console.log("Posted successfully:", data);
         setRefreshKey((prev) => prev + 1);
       } else {
-        console.error('Error posting tweet:', data.message);
+        console.error("Error posting tweet:", data.message);
       }
 
       onClose();
-      setContent('');
-      setImagePath('');
-      setType('tweet');
+      setContent("");
+      setImagePath("");
+      setType("tweet");
     } catch (error) {
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   };
 
@@ -76,12 +87,12 @@ export const HomeCon: FC = () => {
   //typeを日本語に変換するヘルパー関数
   const getLabelForType = (type: string) => {
     switch (type) {
-      case 'tweet':
-        return 'つぶやき';
-      case 'model':
-        return 'モデル募集';
-      case 'camera':
-        return '撮影者募集';
+      case "tweet":
+        return "つぶやき";
+      case "model":
+        return "モデル募集";
+      case "camera":
+        return "撮影者募集";
       default:
         return type;
     }
@@ -90,15 +101,15 @@ export const HomeCon: FC = () => {
   useEffect(() => {
     async function fetchTimeline() {
       try {
-        const response = await fetch('/api/timeline');
+        const response = await fetch("/api/timeline");
         const data = await response.json();
         if (response.ok && data.tweets && Array.isArray(data.tweets)) {
           setTimeline(data.tweets);
         } else {
-          console.error('API did not return the expected format:', data);
+          console.error("API did not return the expected format:", data);
         }
       } catch (error) {
-        console.error('Failed to fetch timeline:', error);
+        console.error("Failed to fetch timeline:", error);
       }
     }
 
@@ -120,6 +131,10 @@ export const HomeCon: FC = () => {
       setFilteredType={setFilteredType}
       onOpen={onOpen}
       onClose={onClose}
+      isDetailModalOpen={isDetailModalOpen}
+      selectedItem={selectedItem}
+      openDetailModal={openDetailModal}
+      closeDetailModal={closeDetailModal}
     />
   );
 };
